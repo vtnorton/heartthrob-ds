f9782d5b6dc45390088c9e507ddbcd170b3d539b
Vítor Norton <vtnorton@outlook.com>
2018-08-31 09:00:54 -0300

    starting fluent design

---
+++ src/Heartthrob/Heartthrob/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Heartthrob.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Index.cshtml");
+        }
+
+        [Route("/2")]
+        public IActionResult InputFields()
+        {
+            return View("~/Views/InputFields.cshtml");
+        }
+    }
+}
