9a756ca655706e572a65b27761051c5260244b5d
Vítor Norton <vtnorton@outlook.com>
2018-09-12 13:12:31 -0300

    Menu

--- src/Heartthrob/Heartthrob/wwwroot/js/site.js
+++ src/Heartthrob/Heartthrob/wwwroot/js/site.js
@@ -1,1 +1,51 @@
-﻿
+﻿$(document).ready(function () {
+    $.fn.clickToggle = function (func1, func2) {
+        var funcs = [func1, func2];
+        this.data('toggleclicked', 0);
+        this.click(function () {
+            var data = $(this).data();
+            var tc = data.toggleclicked;
+            $.proxy(funcs[tc], this)();
+            data.toggleclicked = (tc + 1) % 2;
+        });
+        return this;
+    };
+    
+});
+
+
+function openMenu(menu) {
+    //TODO: Colocar o menu para fechar quando clica fora
+    //TODO: colocar o menu para abrir numa posição conveniente caso o dev coloque no canto superior direito
+    $(menu).slideToggle("fast");
+}
+//function openMenu(menuparaabrir, mensageiro) {
+//    $(menuparaabrir).slideToggle("normal", function () {
+//        if ($(mensageiro).css("font-weight") == "600") {
+//            $(mensageiro).css("font-weight", "300");
+//            //$(mensageiro + " .menu-drop").css("background-position-x", "-84px !important");
+//        } else {
+//            $(mensageiro).css("font-weight", "600");
+//            //$(mensageiro + " .menu-drop").css("background-position-x", "-56px !important");
+//        }
+//    });
+//    //TODO: mudar o ícone para seta para cima
+//};
+
+function backStep(destino, mensageiro) {
+    $(mensageiro).removeClass("view-opened");
+    $(mensageiro).addClass("view-alarm");
+    $(mensageiro).addClass("view-closed", 1000);
+
+    $(destino).removeClass("view-closed", 1000);
+    $(destino).addClass("view-opened");
+}
+
+function nextStep(destino, mensageiro) {
+    $(mensageiro).addClass("view-passed");
+    $(mensageiro).removeClass("view-opened");
+    $(mensageiro).addClass("view-closed", 1000);
+
+    $(destino).removeClass("view-closed", 1000);
+    $(destino).addClass("view-opened");
+}
