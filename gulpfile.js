5a2a59a9c0a968d5714f80ade520c7adeb4ccc29
Vítor Norton <vtnorton@outlook.com>
2018-09-24 14:42:58 -0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
 gulp.task('minify-css', () => {
@@ -8,12 +9,24 @@ gulp.task('minify-css', () => {
     .pipe(rename({
         suffix: '.min'
       }))  
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('minify-js', function() {
+  return gulp.src('src/js/*.js')
+      .pipe(concat('heartthrob.js'))
+      .pipe(rename('heartthrob.min.js'))
+      .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('copy-files', () => {
     return gulp.src('src/css/**')
-      .pipe(gulp.dest('build'));
+      .pipe(gulp.dest('build/css'));
   });
 
-  gulp.task('build', ['minify-css', 'copy-files']);
+  gulp.task('copy-files-js', () => {
+    return gulp.src('src/js/**')
+      .pipe(gulp.dest('build/js'));
+  });
+
+  gulp.task('build', ['minify-css', 'minify-js', 'copy-files', 'copy-files-js']);
