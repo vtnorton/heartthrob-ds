80d9db82dd6690cf53ec09113a0b7c1f1a8d3cca
Vítor Norton <vtnorton@outlook.com>
2018-10-17 10:39:43 -0300

    Função para preencher dados do CEP

--- src/js/heartthrob.js
+++ src/js/heartthrob.js
@@ -328,3 +328,32 @@ function allTheSame(array) {
         return element === first;
     });
 }
+
+function pesquisarCep(valor) {
+    var cep = valor.replace(/\D/g, '');
+    if (cep != "") {
+        var validacep = /^[0-9]{8}$/;
+        if (validacep.test(cep)) {
+            var script = document.createElement('script');
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=preencherdados';
+            document.body.appendChild(script);
+        }
+        else {
+            alert("Formato de CEP inválido.");
+        }
+    }
+};
+
+function preencherdados(conteudo) {
+    if (!("erro" in conteudo)) {
+        document.getElementById('rua').value = conteudo.logradouro;
+        document.getElementById('bairro').value = conteudo.bairro;
+        document.getElementById('cidade').value = conteudo.localidade;
+
+        var dropdownlist = $("#estado").data("kendoDropDownList");
+        dropdownlist.select(function (dataItem) {
+            return dataItem.sigla == conteudo.uf;
+        });
+        dropdownlist.trigger("change");
+    }
+}
