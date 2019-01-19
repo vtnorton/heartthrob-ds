589ca09f2d7dbd2b1330b44ed7b053152f4bcde8
Vítor Norton <vtnorton@outlook.com>
2019-01-19 02:49:15 -0200

    getUrlParameter

--- src/js/heartthrob.js
+++ src/js/heartthrob.js
@@ -371,3 +371,18 @@ function preencherdados(conteudo) {
 function CellCloseScript(e) {
     this.saveChanges();
 }
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
