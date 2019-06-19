6ee4684b7328f146d3f670a27338492e8d0f125c
Norton <norton.tavares@lambda3.com.br>
2019-06-19 17:27:38 -0300

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,36 @@
-let gulp = require('gulp');
-let cleanCSS = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
+let gulp = require('gulp')
+let cleanCSS = require('gulp-clean-css')
+var concat = require('gulp-concat')
+var rename = require('gulp-rename')
 
 gulp.task('minify-css', () => {
   return gulp.src('src/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({
-        suffix: '.min'
-      }))  
+    .pipe(rename({ suffix: '.min' }))  
+    .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('minify-js', function () {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('heartthrob.js'))
+    .pipe(rename('heartthrob.min.js'))
+    .pipe(gulp.dest('build/js'))
+})
+
+gulp.task('copy-files', () => {
+  return gulp.src('src/css/**')
     .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('minify-js', function() {
-  return gulp.src('src/js/*.js')
-      .pipe(concat('heartthrob.js'))
-      .pipe(rename('heartthrob.min.js'))
-      .pipe(gulp.dest('build/js'));
-});
+gulp.task('copy-files-img', () => {
+  return gulp.src('src/img/**')
+    .pipe(gulp.dest('build/img'))
+})
 
-gulp.task('copy-files', () => {
-    return gulp.src('src/css/**')
-      .pipe(gulp.dest('build/css'));
-  });
-  gulp.task('copy-files-img', () => {
-    return gulp.src('src/img/**')
-      .pipe(gulp.dest('build/img'));
-  });
+gulp.task('copy-files-js', () => {
+  return gulp.src('src/js/**')
+    .pipe(gulp.dest('build/js'))
+})
 
-  gulp.task('copy-files-js', () => {
-    return gulp.src('src/js/**')
-      .pipe(gulp.dest('build/js'));
-  });
-
-  gulp.task('build', ['minify-css', 'minify-js', 'copy-files', 'copy-files-js', 'copy-files-img']);
-  gulp.task('default', ['build']);
+gulp.task('build', gulp.parallel('minify-css', 'minify-js', 'copy-files-js', 'copy-files-img'))
+gulp.task('default', gulp.parallel('build'))
