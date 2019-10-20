9b2baa62875d67683735d30093b9a42f4f13d627
Vítor Norton <vtnorton@outlook.com>
2019-10-20 19:57:57 -0300

    scripts for building the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,16 @@
 let gulp = require('gulp')
-let cleanCSS = require('gulp-clean-css')
 var concat = require('gulp-concat')
 var rename = require('gulp-rename')
 var merge = require('merge-stream')
 var minifyjs = require('gulp-js-minify')
 var sass = require('gulp-sass')
+var removeEmptyLines = require('gulp-remove-empty-lines')
 
 var deps = {
 	'heartthrob-vision': {
 		'build/js/**': ''
 	}
 }
-
-gulp.task('sass', function () {
-	return gulp.src('src/sass/main.scss')
-		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-		.pipe(gulp.dest('build/css/'))
-		.pipe(gulp.dest('../Heartthrob-docs/heartthrob-docs/wwwroot/css/'))
-		// using this last gulp.dest() to test on the docs while it's beeing develop, a better solution will be provided.
-})
-
-gulp.task('watch', function () {
-	gulp.watch('src/scss/*.scss', gulp.series('sass'))
-})
 
 gulp.task('restore', async () => {
 	var streams = []
@@ -38,11 +26,29 @@ gulp.task('restore', async () => {
 	return merge(streams)
 })
 
-gulp.task('minify-css', () => {
-	return gulp.src('src/css/*.css')
-		.pipe(cleanCSS({ compatibility: 'ie8' }))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest('build/css'))
+/* building sass */
+gulp.task('build-sass-normal', function () {
+	return gulp.src('src/sass/main.scss')
+		.pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+		.pipe(removeEmptyLines({ removeComments: true }))
+		.pipe(rename('heartthrob.css'))
+		.pipe(gulp.dest('build/css/'))
+})
+
+gulp.task('build-sass-min', function () {
+	return gulp.src('src/sass/main.scss')
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(rename('heartthrob.min.css'))
+		.pipe(gulp.dest('build/css/'))
+})
+
+gulp.task('build-sass-dev', function () {
+	return gulp.src('src/sass/main.scss')
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(removeEmptyLines({ removeComments: true }))
+		.pipe(rename('heartthrob.css'))
+		.pipe(gulp.dest('../Heartthrob-docs/heartthrob-docs/wwwroot/css/'))
+		// using this last gulp.dest() to test on the docs while it's beeing develop, a better solution will be provided.
 })
 
 gulp.task('minify-js', function () {
@@ -63,15 +69,15 @@ gulp.task('copy-files-img', () => {
 		.pipe(gulp.dest('build/img'))
 })
 
-gulp.task('copy-files-css', () => {
-	return gulp.src('src/css/**')
-		.pipe(gulp.dest('build/css'))
-})
-
 gulp.task('copy-files-js', () => {
 	return gulp.src('src/js/**')
 		.pipe(gulp.dest('build/js'))
 })
 
-gulp.task('build', gulp.parallel('restore', 'copy-files-css', 'minify-css', 'minify-js', 'copy-files-js', 'copy-files-img'))
+gulp.task('build-js', gulp.parallel('restore', 'minify-js', 'copy-files-js'))
+gulp.task('build-img', gulp.parallel('copy-files-img'))
+gulp.task('build-sass', gulp.parallel('build-sass-normal', 'build-sass-min'))
+
+gulp.task('build', gulp.parallel('build-img', 'build-js', 'build-sass'))
+gulp.task('build-dev', gulp.parallel('build', 'build-sass-dev'))
 gulp.task('default', gulp.parallel('build'))
