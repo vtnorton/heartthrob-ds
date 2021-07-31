2785b72c5863bd638301cb83c52963bd5edea862
Vitor Norton <vtnorton@outlook.com>
2021-07-31 00:30:15 -0300

    removed gulp

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import scss from 'rollup-plugin-scss'
+import postcss from 'postcss'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+import perfectionist from 'perfectionist'
+
+const buildPath = './build/'
+const inputFile = 'src/index.js'
+const defaultJsBuildSettings = [
+	{
+		file: buildPath + 'output.js',
+		format: 'cjs',
+		sourcemap: false,
+	},
+]
+
+export default [
+	{
+		input: inputFile,
+		output: defaultJsBuildSettings,
+		plugins: [
+			scss({
+				output: buildPath + 'heartthrob.min.css',
+				failOnError: true,
+				processor: () => postcss([
+					autoprefixer(),
+					cssnano({
+						preset: 'default'
+					})
+				]),
+			}),
+		],
+	},
+	{
+		input: inputFile,
+		output: defaultJsBuildSettings,
+		plugins: [
+			scss({
+				output: buildPath + 'heartthrob.css',
+				failOnError: true,
+				processor: () => postcss([
+					autoprefixer(),
+					perfectionist({
+						format: 'compact'
+					})
+				]),
+			}),
+		],
+	},
+]
